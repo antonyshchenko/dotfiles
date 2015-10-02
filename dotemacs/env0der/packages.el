b0d43681abac305d74c90d17275ffcbb3a029016
env0der <anton.onishchenko@gmail.com>
2015-10-02 13:31:52 +0200

    Emacs: JS mode for .json files

--- dotemacs/env0der/packages.el
+++ dotemacs/env0der/packages.el
@@ -370,7 +370,8 @@
   (use-package js2-mode
     :init
     (progn
-      (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+      (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+      (add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode)))
     :config
     (progn
       (add-hook 'js2-init-hook (lambda ()
